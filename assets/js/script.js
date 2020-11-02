a53d25bc97d8c9264d2a2668f845c824113e2d4d
mkalidas-prdxn <71173672+mkalidas-prdxn@users.noreply.github.com>
2020-11-02 16:30:45 +0530

    Added hamburger menu functionality in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,12 @@
-/* Author: 
-
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//=> Hamburger menu functionality
+var hamburger = document.getElementById("hamburger"),
+    hamMenuOpen = document.querySelector(".menu"),
+    menuBarBackground = document.querySelector(".main-bar-url");
+hamburger.addEventListener("click", cross);
+//Hambuger functiom to open and close menu
+function cross() {
+  document.body.classList.toggle("body"); //to stop scrolling when menu open
+  hamburger.classList.toggle("activeHam");
+  hamMenuOpen.classList.toggle("hamMenuOpen");
+  menuBarBackground.classList.toggle("menuBarBackground");
+}
